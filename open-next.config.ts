f1f442c2827edcf0b212316c54acc70a560f8cee
MdSalman2022 <mehedi.salman102@gmail.com>
2026-03-11 18:04:34 +0600

    updated config

---
+++ open-next.config.ts
@@ -0,0 +1,5 @@
+import { defineCloudflareConfig } from "@opennextjs/cloudflare";
+
+export default defineCloudflareConfig({
+  // Basic configuration to fix the missing open-next.config.ts error.
+});
